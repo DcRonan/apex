b0dd8deb544484faf6a33711d5020ed51f7ec86f
Daniel Ronan <danielconnorronan@gmail.com>
2021-04-10 22:29:19 +0100

    style sport feed

--- src/containers/SportList.js
+++ src/containers/SportList.js
@@ -6,22 +6,21 @@ const SportList = () => {
   const sports = useSelector(state => state.sport.data);
 
   return (
-    <div className="h-full">
+    <div className="h-full pb-16">
       <div className="p-4 bg-lightblue text-white font-nimbuslight w-full text-center">
         <h1 className="text-2xl">Feed</h1>
       </div>
-      <section>
+      <section className="space-y-4">
         {sports.map(el => (
-          <div key={Math.random().toString(36).substr(2, 9)}>
-            <h2>{el.name}</h2>
-            <p>
-              <span>{el.hours}</span>
-              :
-              <span>{el.minutes}</span>
-              :
-              <span>{el.seconds}</span>
-            </p>
-            <div>{dateFormat(el.created_at, 'yyyy-MM-dd')}</div>
+          <div key={Math.random().toString(36).substr(2, 9)} className="py-4 px-5 bg-gray-300 bg-opacity-30 border-b-2 border-t-2 flex justify-between items-center">
+            <div>
+              <h2 className="font-nimbusbold">{el.name}</h2>
+              <p className="text-xs">{dateFormat(el.created_at, 'yyyy-MM-dd')}</p>
+            </div>
+            <div className="font-nimbusbold text-green-700">
+              {el.distance}
+              <span className="p-1">mi</span>
+            </div>
           </div>
         ))}
       </section>

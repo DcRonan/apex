1aca55f21a4663ea36d5a04d63182506d0ff8807
Daniel Ronan <danielconnorronan@gmail.com>
2021-04-12 21:53:17 +0100

    finish single sport container

--- src/containers/SingleSport.js
+++ src/containers/SingleSport.js
@@ -4,29 +4,64 @@ import dateFormat from 'dateformat';
 
 const SportList = () => {
   const singleSport = useSelector(state => state.singleSport.data);
+  const title = singleSport[0].name;
+  console.log(singleSport);
 
   return (
-    <div className="h-full pb-16">
+    <div className="h-screen pb-20">
       <div className="p-4 bg-lightblue text-white font-nimbuslight w-full text-center">
-        <h1 className="text-2xl">Feed</h1>
+        <h1 className="text-2xl">{title}</h1>
       </div>
-      <section className="space-y-4">
+      <section className="space-y-6 mx-6 text-center pt-10">
         {singleSport.map(el => (
-          <div
-            key={Math.random().toString(36).substr(2, 9)}
-            className="py-4 px-5 bg-gray-300 bg-opacity-30 border-b-2 border-t-2 flex justify-between items-center"
-          >
-            <div>
-              <h2 className="font-nimbusbold">{el.name}</h2>
-              <p className="text-xs">
-                {dateFormat(el.created_at, 'yyyy-MM-dd')}
-              </p>
+          <>
+            <div
+              key={Math.random().toString(36).substr(2, 9)}
+              className="py-6 px-5 bg-gray-300 bg-opacity-30 relative"
+            >
+              <div className="text-xs text-lightgrey font-nimbuslight absolute top-2 left-2">
+                Distance
+              </div>
+              <div className="font-nimbusbold text-green-700 text-2xl">
+                <div>
+                  <span>{el.distance}</span>
+                  <span className="pl-1">mi</span>
+                </div>
+              </div>
             </div>
-            <div className="font-nimbusbold text-green-700">
-              {el.distance}
-              <span className="p-1">mi</span>
+            <div className="py-6 px-5 bg-gray-300 bg-opacity-30 relative">
+              <div className="text-xs text-lightgrey font-nimbuslight absolute top-2 left-2">
+                Time
+              </div>
+              <div className="font-nimbusbold text-red-700 text-2xl">
+                <div>
+                  <span>{el.hours}</span>
+                  :
+                  <span>{el.minutes}</span>
+                  :
+                  <span>{el.seconds}</span>
+                </div>
+              </div>
             </div>
-          </div>
+            <div className="py-6 px-5 bg-gray-300 bg-opacity-30 relative">
+              <div className="text-xs text-lightgrey font-nimbuslight absolute top-2 left-2">
+                Notes
+              </div>
+              <div className="font-nimbuslight text-black text-base">
+                <div>
+                  <p>{el.notes}</p>
+                </div>
+              </div>
+            </div>
+            <div className="py-6 px-5 bg-gray-300 bg-opacity-30 relative">
+              <div className="text-xs text-lightgrey font-nimbuslight absolute top-2 left-2">
+                Time
+              </div>
+              <div className="font-nimbusbold text-black text-lg">
+                <div>{dateFormat(el.created_at, 'yyyy-MM-dd')}</div>
+              </div>
+            </div>
+          </>
         ))}
       </section>
     </div>
